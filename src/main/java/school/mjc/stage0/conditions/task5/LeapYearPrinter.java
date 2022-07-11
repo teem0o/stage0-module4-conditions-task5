ca19d55cb6cbb8e7a8520241df7687dbda8f039f
temo <chxaidzetemo@gmail.com>
2022-07-12 00:24:11 +0400

    fghd

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+//        int remainder= year % 400 == 0 || year % 100 == 0 || year % 4 == 0 ? 1 : 0;
+        int remainder = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)?1:0;
+
+        switch (remainder) {
+            case 1:
+                System.out.println("leap");
+                break;
+            case 0:
+                System.out.println("not leap");
+                break;
+            default:
+                System.out.println();
+                break;
+        }
     }
 }
+
